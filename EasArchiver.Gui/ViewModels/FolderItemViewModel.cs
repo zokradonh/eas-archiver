bfb506d9a0191a431d98344930742ac88eb808df
ZokRadonh <az@zok.xyz>
2026-05-24 11:56:08 +0200

    UI changes, folder icons, sync button

--- EasArchiver.Gui/ViewModels/FolderItemViewModel.cs
+++ EasArchiver.Gui/ViewModels/FolderItemViewModel.cs
@@ -7,6 +7,19 @@ public partial class FolderItemViewModel : ObservableObject
     [ObservableProperty] private bool isSelected = true;
 
     public string Path { get; }
+    public string DisplayName { get; }
+    public string ParentPath { get; }
+    public int Depth { get; }
+    public double IndentMargin => Depth * 16.0;
 
-    public FolderItemViewModel(string path) => Path = path;
+    public FolderItemViewModel(string path)
+    {
+        Path = path;
+        var segments = path.Split('/');
+        Depth = segments.Length - 1;
+        DisplayName = segments[^1];
+        ParentPath = Depth > 0
+            ? string.Join(" / ", segments[..^1]) + " /"
+            : "";
+    }
 }
